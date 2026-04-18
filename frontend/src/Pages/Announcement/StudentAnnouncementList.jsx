20fa9b75709e6a9b4273d6ba6373dfd7266b6d5d
manjitha kavishan <manjikavi8@gmail.com>
2026-04-18 23:39:18 +0530

    change port

--- frontend/src/Pages/Announcement/StudentAnnouncementList.jsx
+++ frontend/src/Pages/Announcement/StudentAnnouncementList.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../../Components/Navbar/Navbar";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://localhost:5000/api";
 const authHeader = () => {
   const token = localStorage.getItem("token");
   return token ? { Authorization: `Bearer ${token}` } : {};
